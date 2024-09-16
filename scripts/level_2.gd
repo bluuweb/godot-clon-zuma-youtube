b5c97d661cbde9c32dc79aac237b84a0cfd692b7
bluuweb <contacto@bluuweb.cl>
2024-09-16 17:54:25 -0300

    casi listo

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -21,6 +21,8 @@ func create_ball():
 func add_ball_to_path2D(ball_ref, new_ball):
 	#print(ball_ref.id)
 	#print(new_ball)
+	new_ball.id = ball_ref.id
+	
 	for item in array_follows:
 		if item.id < ball_ref.id:
 			item.ball.progress_ratio += 0.01
@@ -28,14 +30,45 @@ func add_ball_to_path2D(ball_ref, new_ball):
 			item.id += 1
 			item.ball.id = item.id
 	
-	new_ball.id = ball_ref.id
-	array_follows.append({
+	var new_ball_object = {
 		"id": new_ball.id,
 		"ball": new_ball,
 		"color": new_ball.get_color()
-	})
+	}
+	array_follows.append(new_ball_object)
 	
-	#print(array_follows)
-
+	var array_equals_color = []
+	array_equals_color.append(new_ball_object)
+	
+	var array_find = []
+	for i in range((new_ball.id - 1), 0, -1):
+		array_follows.filter(func (x):
+			if x.id == i:
+				array_find.append(x)
+		)
+	for item in array_find:
+		if item.color == new_ball.get_color():
+			array_equals_color.append(item)
+		else: break
+	
+	array_find = []
+	for i in range(new_ball.id + 1, array_follows.size(), 1):
+		array_follows.filter(func (x):
+			if x.id == i:
+				array_find.append(x)
+		)
+	for item in array_find:
+		if item.color == new_ball.get_color():
+			array_equals_color.append(item)
+		else: break
+	
+	if array_equals_color.size() >= 3:
+		for item in array_equals_color:
+			array_follows.erase(item)
+			path_2d.remove_child(item.ball)
+			# TODO: falta cambiar los ids, ya que se eliminaros algunos, para mantener la coincistencia
+	
+	
+	
 func _on_timer_create_ball_timeout() -> void:
 	create_ball()
